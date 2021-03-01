58e4e05ec27097219ab1019e9048ee07b3bd287e
Dan Losh <losh.dan@gmail.com>
2021-03-01 17:26:51 -0500

    Added optional chaining plugin for babel. Is only supported in JS, and is not supported in Vue 2 Template. https://babeljs.io/docs/en/babel-plugin-proposal-optional-chaining

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ header }}</h1>
+    <h2>{{ nullHeader }}</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -66,7 +67,20 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String,
+    text: Object,
+  },
+  computed: {
+    header() {
+      return this.text?.header;
+    },
+    /**
+     * Example how now optional chaining protects against accessing keys
+     * on nullish (null or undefined) properties.
+     * See for yourself by removing the `?`.
+     */
+    nullHeader() {
+      return this.text.thisIsUndefined?.header;
+    },
   },
 };
 </script>
